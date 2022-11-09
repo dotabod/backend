42afdad2bbc02483b069622287c089292c59e90c
Matt <Geczy@users.noreply.github.com>
2022-11-08 19:48:04 -0600

    wtf

--- src/dota/index.js
+++ src/dota/index.js
@@ -63,8 +63,7 @@ function updateGamestate(req, res, next) {
 }
 
 function newData(req, res) {
-  const token = req.body?.auth?.token
-  console.log(!req.body?.map?.customgamename, req.client.socketinfo)
+  console.log(!req.body, req.client.socketinfo)
 
   io.to(req.client.socketinfo.sockets).emit('state', req.body?.map?.game_state || 'DISCONNECTED')
   res.end()
