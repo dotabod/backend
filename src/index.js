557f79863019370be5e315e7527127dc80956fe1
Matt <Geczy@users.noreply.github.com>
2022-11-12 01:55:42 -0600

    disable twitch for now

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import * as dota from './dota/index.js'
-import * as twitch from './twitch/index.js'
+import * as dotagsi from './dota/gsi.js'
+import * as dotasocket from './dota/socket.js'
+// import * as twitch from './twitch/index.js'
