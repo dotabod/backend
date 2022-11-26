ef6d3fecc1c092dbd3d3861cf683c499209f9a22
Geczy <Geczy@users.noreply.github.com>
2022-11-26 09:40:17 -0600

    increase retries

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,9 +2,8 @@ import axios from 'axios'
 import axiosRetry from 'axios-retry'
 
 axiosRetry(axios, {
-  retries: 5, // number of retries
+  retries: 7, // number of retries
   retryDelay: (retryCount) => {
-    console.log(`retry attempt: ${retryCount}`)
     return retryCount * 4000 // time interval between retries
   },
   retryCondition: (error) => {
