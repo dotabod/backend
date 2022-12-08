b2f5babd5193f2d9c86f4ae6b57de73b59f22eeb
Matt <Geczy@users.noreply.github.com>
2022-12-08 12:14:29 -0600

    add matchid command

---
+++ src/twitch/commands/match.ts
@@ -0,0 +1,22 @@
+import commandHandler, { MessageType } from './CommandHandler.js'
+
+import { chatClient } from './index.js'
+
+commandHandler.registerCommand('match', {
+  aliases: ['matchid'],
+  permission: 0,
+  cooldown: 15000,
+  handler: (message: MessageType, args: string[]) => {
+    const {
+      channel: { name: channel, client },
+    } = message
+    const matchid = client.gsi?.gamestate?.map?.matchid
+
+    if (!matchid) {
+      void chatClient.say(channel, 'Match ID: Unknown')
+      return
+    }
+
+    void chatClient.say(channel, `Match ID: ${matchid}`)
+  },
+})
