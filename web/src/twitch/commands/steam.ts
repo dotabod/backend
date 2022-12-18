e486227bb48117630707df4ad64a8502d0283d31
Matt <Geczy@users.noreply.github.com>
2022-12-18 13:52:30 -0600

    fix: tell us real steamid

--- web/src/twitch/commands/steam.ts
+++ web/src/twitch/commands/steam.ts
@@ -10,11 +10,10 @@ commandHandler.registerCommand('steam', {
       channel: { name: channel, client },
     } = message
 
-    // TODO: whispers do not work via chatClient, have to use helix api
-    // helix api rate limits you to 40 unique whispers a day though ?? so just not gonna do it
-    void chatClient.say(
-      channel,
-      `https://steamid.xyz/${client.steam32Id ?? ' Unknown steam ID. Play a match first!'}`,
-    )
+    if (client.steam32Id && Number(client.steam32Id)) {
+      void chatClient.say(channel, `https://steamid.xyz/${client.steam32Id.toString()}`)
+    }
+
+    void chatClient.say(channel, 'Unknown steam ID. Play a match first!')
   },
 })
