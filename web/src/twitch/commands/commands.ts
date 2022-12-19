91dd1338a51a731777cbb2b59cd7f4cad25b9bc5
Matt <Geczy@users.noreply.github.com>
2022-12-19 11:10:50 -0600

    feat: only show commands enabled in !commands
    closes #51

--- web/src/twitch/commands/commands.ts
+++ web/src/twitch/commands/commands.ts
@@ -1,3 +1,4 @@
+import { getValueOrDefault } from '../../db/settings.js'
 import { chatClient } from '../index.js'
 import commandHandler, { MessageType } from './CommandHandler.js'
 
@@ -12,9 +13,27 @@ commandHandler.registerCommand('commands', {
     // TODO: respond only with commands that are enabled for the channel
     // TODO: only commands user can use
 
-    void chatClient.say(
-      channel,
-      `Available commands: ${Array.from(commandHandler.commands.keys()).join(' · ')}`,
-    )
+    // Find commands where we have user permission
+    const filtered = [...commandHandler.commands]
+      .filter(([k, v]) => v.permission <= message.user.permission)
+      .filter(([k, v]) => {
+        if (v.dbkey) {
+          return getValueOrDefault(v.dbkey, message.channel.settings)
+        }
+        return true
+      })
+      .map(([k, v]) => ({
+        command: k,
+        permission: v.permission,
+      }))
+      .sort((a, b) => a.command.localeCompare(b.command))
+
+    const everyone = filtered.filter((v) => v.permission === 0).map((v) => v.command)
+    const others = filtered.filter((v) => v.permission > 0).map((v) => v.command)
+
+    void chatClient.say(channel, `Everyone can use: ${everyone.join(' · ')}.`)
+
+    if (others.length === 0) return
+    void chatClient.say(channel, `Mod only commands: ${others.join(' · ')}.`)
   },
 })
