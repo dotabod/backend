3fae9721d7d7348ed76ca7ed1f21bcd2552354df
Geczy <Geczy@users.noreply.github.com>
2022-12-24 10:46:43 -0500

    fix: handle username changes from frontend
    same for follow count, refresh each time they login
    
    as it should be

--- twitch-chat-listener/src/db/watcher.ts
+++ twitch-chat-listener/src/db/watcher.ts
@@ -1,30 +1,8 @@
 import { User } from '../../prisma/generated/postgresclient/index.js'
 import { chatClient } from '../index.js'
 import supabase from './supabase.js'
-import { updateFollowForUser } from './update-follows.js'
-import { updateUsernameForId } from './update-username.js'
 
 const channel = supabase.channel('twitch-changes')
-
-async function handleNewUser(user: User) {
-  try {
-    await updateFollowForUser(user.id)
-    console.log('Follows updated for new user')
-  } catch (e) {
-    console.error('Error updating follows for new user', e)
-  }
-
-  try {
-    const realUsername = await updateUsernameForId(user.id)
-    console.log('Username updated for new user')
-    await chatClient.join(realUsername ?? user.name)
-    console.log('[SUPABASE]', 'Joined channel new name', realUsername ?? user.name)
-  } catch (e) {
-    console.error('Error updating username for new user', e)
-    await chatClient.join(user.name)
-    console.log('[SUPABASE]', 'Joined channel old name', user.name)
-  }
-}
 
 channel
   .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'users' }, (payload) => {
@@ -34,7 +12,7 @@ channel
 
     const user = payload.new as User
     console.log('[SUPABASE]', 'New user to send bot to: ', user.name)
-    void handleNewUser(user)
+    void chatClient.join(user.name)
   })
   .subscribe((status, err) => {
     if (status === 'SUBSCRIBED') {
