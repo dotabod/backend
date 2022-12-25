8c28a9340dd5eec278d5b7d8e98cb9be09016120
Geczy <Geczy@users.noreply.github.com>
2022-12-25 12:38:14 -0500

    fix: check the displayname event instead of insert event

--- twitch-chat-listener/src/db/watcher.ts
+++ twitch-chat-listener/src/db/watcher.ts
@@ -5,17 +5,20 @@ import supabase from './supabase.js'
 const channel = supabase.channel('twitch-changes')
 
 channel
-  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'users' }, (payload) => {
+  .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'users' }, (payload) => {
     if (process.env.NODE_ENV !== 'production') {
       return
     }
 
-    const user = payload.new as User
-    console.log('[SUPABASE] New user to send bot to: ', user.name)
-    try {
-      void chatClient.join(user.name)
-    } catch (e) {
-      console.error('[SUPABASE] Error joining channel: ', e)
+    const oldUser = payload.old as User
+    const newUser = payload.new as User
+    if (!oldUser.displayName && newUser.displayName) {
+      console.log('[SUPABASE] New user to send bot to: ', newUser.name)
+      try {
+        void chatClient.join(newUser.name)
+      } catch (e) {
+        console.error('[SUPABASE] Error joining channel: ', e)
+      }
     }
   })
   .subscribe((status, err) => {
