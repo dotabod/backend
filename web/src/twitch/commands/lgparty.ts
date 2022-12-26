7bc33b95999ebd5d9fe35ecf6573aa90bc75b2c4
Geczy <Geczy@users.noreply.github.com>
2022-12-26 13:33:25 -0500

    fix: new cmd again

--- web/src/twitch/commands/lgparty.ts
+++ web/src/twitch/commands/lgparty.ts
@@ -6,7 +6,7 @@ import commandHandler, { MessageType } from '../lib/CommandHandler.js'
 
 function calculateMmr(currentMmr: number, wasParty: boolean, didWin: boolean) {
   const mmrChange = didWin ? 10 : -10
-  return wasParty ? currentMmr + mmrChange : currentMmr - mmrChange
+  return wasParty ? currentMmr - mmrChange : currentMmr + mmrChange
 }
 
 commandHandler.registerCommand('lgparty', {
