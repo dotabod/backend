63e38c71654f8bc602e5b814d9e65ea77c37c9d1
Geczy <Geczy@users.noreply.github.com>
2022-12-28 13:02:14 -0500

    dev: make findItem work for multiple

--- web/src/dota/lib/findItem.ts
+++ web/src/dota/lib/findItem.ts
@@ -1,19 +1,22 @@
 import { Item, Packet } from '../../types.js'
 
-export function findItem(itemName: string, searchStashAlso: boolean, data?: Packet) {
+export function findItem(itemName: string | string[], searchStashAlso: boolean, data?: Packet) {
   if (!data?.items) return false
 
   // Should always be 17 unless they're not in a match
   if (Object.keys(data.items).length !== 17) return false
 
+  const itemNames = Array.isArray(itemName) ? itemName : [itemName]
+
   // This checks backpack only, not fountain stash cause maybe courrier is bringing it
   const inv = Object.values(data.items)
-  const item: Item | undefined = inv
+
+  const items: Item[] = inv
     .slice(0, searchStashAlso ? 9 : 6)
-    .find((item: Item) => item.name === itemName)
+    .filter((item: Item) => itemNames.includes(item.name))
 
-  // Doesn't have a midas
-  if (!item) return false
+  // Doesn't have this item
+  if (!items.length) return false
 
-  return item
+  return items.length === 1 ? items[0] : items
 }
