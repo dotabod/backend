593c3553b598ae0123a1c044b647ef4135908e41
Geczy <Geczy@users.noreply.github.com>
2022-12-29 08:43:01 -0500

    disable !hero command

--- web/src/twitch/commands/hero.ts
+++ web/src/twitch/commands/hero.ts
@@ -1,8 +1,6 @@
 import { DBSettings } from '../../db/settings.js'
 import getHero from '../../dota/lib/getHero.js'
 import { isPlayingMatch } from '../../dota/lib/isPlayingMatch.js'
-import axios from '../../utils/axios.js'
-import { logger } from '../../utils/logger.js'
 import { chatClient } from '../index.js'
 import commandHandler, { MessageType } from '../lib/CommandHandler.js'
 
@@ -36,34 +34,9 @@ commandHandler.registerCommand('hero', {
       return
     }
 
-    const allTime = args[0] === 'all'
-
-    axios(
-      `https://api.opendota.com/api/players/${client.steam32Id}/wl/?hero_id=${hero.id}&having=1${
-        allTime ? '' : '&date=30'
-      }`,
+    void chatClient.say(
+      channel,
+      `!hero command disabled because opendota blocked us for having too many users Sadge`,
     )
-      .then(({ data }: { data: { win: number; lose: number } }) => {
-        const wl = data.win + data.lose
-        const winrate = !wl ? 0 : Math.round((data.win / wl) * 100)
-        const timePeriod = allTime ? 'in lifetime' : 'in 30d'
-
-        if (wl > 0) {
-          void chatClient.say(
-            channel,
-            `Winrate: ${winrate}% as ${hero.localized_name} ${timePeriod} of ${wl} matches.`,
-          )
-          return
-        }
-
-        if (!wl) {
-          void chatClient.say(channel, `No matches played as ${hero.localized_name} ${timePeriod}.`)
-          return
-        }
-      })
-      .catch((e) => {
-        void chatClient.say(channel, `Playing ${hero.localized_name}`)
-        logger.info('could not find wl, weirdge', e?.data)
-      })
   },
 })
