7cd44b497136f2b7769705c9187b64103a1f5873
Geczy <Geczy@users.noreply.github.com>
2022-12-30 18:02:15 -0500

    i18n: use _many

--- web/i18next-parser.config.js
+++ web/i18next-parser.config.js
@@ -32,7 +32,7 @@ export default {
   lineEnding: 'auto',
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: ['en', 'ru'],
+  locales: ['en', 'ru', 'it'],
   // An array of the locales in your applications
 
   namespaceSeparator: ':',
