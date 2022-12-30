50d3837c7d58872e22eda976742625c169db084e
Geczy <Geczy@users.noreply.github.com>
2022-12-30 15:09:25 -0500

    add translator to the chat bot too

--- twitch-chat-listener/src/index.ts
+++ twitch-chat-listener/src/index.ts
@@ -1,4 +1,17 @@
+import { t, use } from 'i18next'
+import FsBackend, { FsBackendOptions } from 'i18next-fs-backend'
 import { Server } from 'socket.io'
+
+await use(FsBackend).init<FsBackendOptions>({
+  initImmediate: false,
+  lng: 'en',
+  fallbackLng: 'en',
+  preload: ['en', 'ru'],
+  defaultNS: 'translation',
+  backend: {
+    loadPath: 'locales/{{lng}}/{{ns}}.json',
+  },
+})
 
 import './db/watcher.js'
 import { getChatClient } from './twitch/lib/getChatClient.js'
@@ -30,7 +43,7 @@ chatClient.onMessage(function (channel, user, text, msg) {
   if (!hasDotabodSocket) {
     // TODO: only commands that we register should be checked here
     if (text === '!ping') {
-      void chatClient.say(channel, 'Servers are rebooting...Try again soon PauseChamp')
+      void chatClient.say(channel, t('rebooting', { lng: 'en' }))
     }
 
     return
