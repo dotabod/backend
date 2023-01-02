e25c421259a6de071de1d99e8b84927392901de3
Matt <Geczy@users.noreply.github.com>
2023-01-02 09:21:51 -0600

    echo >

--- nginx/docker-nginx-certs.sh
+++ nginx/docker-nginx-certs.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p /etc/ssl
-echo "$TLS_CERT" >> /etc/ssl/public.key
-echo "$TLS_KEY" >> /etc/ssl/private.key
+echo "$TLS_CERT" > /etc/ssl/public.key
+echo "$TLS_KEY" > /etc/ssl/private.key
 
 exec "$@"
