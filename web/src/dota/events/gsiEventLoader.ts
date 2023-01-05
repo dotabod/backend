d3d063aef9aaa04010dabb66fe792b52f7df95fb
Matt <Geczy@users.noreply.github.com>
2023-01-04 21:02:59 -0600

    fix: loader in windows

--- web/src/dota/events/gsiEventLoader.ts
+++ web/src/dota/events/gsiEventLoader.ts
@@ -4,8 +4,7 @@ import path from 'path'
 type GSIEventsMap = Record<string, any>
 const gsiEvents: GSIEventsMap = {}
 
-const dirname = path.dirname(new URL(import.meta.url).pathname)
-fs.readdirSync(path.join(dirname, './gsi-events')).forEach((file) => {
+fs.readdirSync(path.resolve('src', 'dota', 'events', 'gsi-events')).forEach((file) => {
   if (file.endsWith('.js') || file.endsWith('.ts')) {
     const gsiEvent = import(`./gsi-events/${file}`)
     gsiEvents[file.slice(0, -3)] = gsiEvent
--- web/src/twitch/commandLoader.ts
+++ web/src/twitch/commandLoader.ts
@@ -4,8 +4,7 @@ import path from 'path'
 type CommandMap = Record<string, any>
 const commands: CommandMap = {}
 
-const dirname = path.dirname(new URL(import.meta.url).pathname)
-fs.readdirSync(path.join(dirname, './commands')).forEach((file) => {
+fs.readdirSync(path.resolve('src', 'twitch', 'commands')).forEach((file) => {
   if (file.endsWith('.js') || file.endsWith('.ts')) {
     const command = import(`./commands/${file}`)
     commands[file.slice(0, -3)] = command
