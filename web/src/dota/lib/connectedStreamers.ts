1dbf0b2d9d6320aec2b886ea1ebb2a7cb76a5ea8
Matt <Geczy@users.noreply.github.com>
2023-01-06 09:42:47 -0600

    just another case of nulls

--- web/src/dota/lib/connectedStreamers.ts
+++ web/src/dota/lib/connectedStreamers.ts
@@ -3,15 +3,15 @@ import { gsiHandlers, twitchIdToToken } from '../index.js'
 
 export default function findUser(token?: string): SocketClient | null {
   if (!token || !gsiHandlers.has(token)) return null
-  return gsiHandlers.get(token)!.client
+  return gsiHandlers.get(token)?.client ?? null
 }
 
-export function findUserByTwitchId(twitchId: string): SocketClient | null {
+export function findUserByTwitchId(twitchId?: string): SocketClient | null {
   if (!twitchId) return null
   if (!twitchIdToToken.has(twitchId)) return null
 
   const token = twitchIdToToken.get(twitchId)
   if (!token || !gsiHandlers.has(token)) return null
 
-  return gsiHandlers.get(token)!.client
+  return gsiHandlers.get(token)?.client ?? null
 }
