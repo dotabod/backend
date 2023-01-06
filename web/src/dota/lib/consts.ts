65b962115e564f8eee08e4dd62d1bcc54cf85f55
Matt <Geczy@users.noreply.github.com>
2023-01-06 14:53:06 -0600

    chore

--- web/src/dota/lib/consts.ts
+++ web/src/dota/lib/consts.ts
@@ -5,25 +5,20 @@ export const playingStates = [
 
 export const dontBlockStates = [
   'DOTA_GAMERULES_STATE_TEAM_SHOWCASE',
+
+  // Adding this strategy to dont block because this state is checked manually
+  // This state actually means all players are locked in and picked
+  // It doesn't mean only the streamer is locked in like I thought
+  // So since all are locked in, everyone can see heroes, and we should unblock all
   'DOTA_GAMERULES_STATE_STRATEGY_TIME',
 ]
 
 export const pickSates = ['DOTA_GAMERULES_STATE_HERO_SELECTION']
 
-// We handle the case of streamer locking in their hero,
-// and this state being true in setupOBSBlockers()
-export const stratStates = ['DOTA_GAMERULES_STATE_STRATEGY_TIME']
-
 export const blockTypes = [
   { type: 'picks', states: pickSates },
   { type: 'playing', states: playingStates },
   { type: 'empty', states: dontBlockStates },
-  // Removing because this state is checked manually
-  // This state actually means all players are locked in and picked
-  // It doesn't mean only the streamer is locked in like I thought
-  // So since all are locked in, everyone can see heroes, and we should unblock
-  // all blockers. Therefore removing this state from the list
-  // { type: 'strategy', states: stratStates },
 ]
 
 export const GLOBAL_DELAY = 7000 // 7s
