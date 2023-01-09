fc269f4fd912439d71ebbaf4db7623f88c44f3f8
Geczy <Geczy@users.noreply.github.com>
2023-01-08 23:41:22 -0600

    do createdAt to undo issue

--- web/src/db/getWL.ts
+++ web/src/db/getWL.ts
@@ -32,7 +32,7 @@ export async function getWL(channelId: string, startDate: Date | null) {
             providerAccountId: channelId,
           },
         },
-        updatedAt: {
+        createdAt: {
           // if its null then only the last 12 hours from now
           // should never be null but just in case
           gte: startDate ?? new Date(new Date().getTime() - 12 * 60 * 60 * 1000),
