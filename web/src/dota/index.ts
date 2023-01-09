602b9be028cf2077dfcba10769f77c9bb71d5056
Matt <Geczy@users.noreply.github.com>
2023-01-09 16:23:28 -0600

    rm newrelic for now from load

--- web/src/dota/index.ts
+++ web/src/dota/index.ts
@@ -1,14 +1,13 @@
 import './events/gsiEventLoader.js'
 
-import RedisClient from '../db/redis.js'
 import { GSIHandler } from './GSIHandler.js'
 import GSIServer from './GSIServer.js'
 
 // Here's where we force wait for the redis to connect before starting the server
 // Server depends on redis to be alive
-const redisClient = RedisClient.getInstance()
-await redisClient.connectClient()
-await redisClient.connectSubscriber()
+// const redisClient = RedisClient.getInstance()
+// await redisClient.connectClient()
+// await redisClient.connectSubscriber()
 
 // Then setup the dota gsi server & websocket server
 export const server = new GSIServer()
