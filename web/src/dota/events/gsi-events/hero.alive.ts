c6722ff82b53a527f9da5d767a034a2cfda9d464
Matt <Geczy@users.noreply.github.com>
2023-01-11 11:03:36 -0600

    refactor: we can move aegis drop to deaths too

--- web/src/dota/events/gsi-events/hero.alive.ts
+++ web/src/dota/events/gsi-events/hero.alive.ts
@@ -1,10 +1,4 @@
-import { t } from 'i18next'
-
-import { DBSettings, getValueOrDefault } from '../../../db/settings.js'
 import { GSIHandler } from '../../GSIHandler.js'
-import { server } from '../../index.js'
-import { findItem } from '../../lib/findItem.js'
-import getHero from '../../lib/getHero.js'
 import { isPlayingMatch } from '../../lib/isPlayingMatch.js'
 import eventHandler from '../EventHandler.js'
 
@@ -13,12 +7,6 @@ eventHandler.registerEvent(`hero:alive`, {
     if (!dotaClient.client.stream_online) return
     if (!isPlayingMatch(dotaClient.client.gsi)) return
 
-    // Case one, we had aegis, and we die with it. Triggers on an aegis death
-    if (!alive && dotaClient.aegisPickedUp?.playerId === dotaClient.playingHeroSlot) {
-      dotaClient.aegisPickedUp = undefined
-      server.io.to(dotaClient.getToken()).emit('aegis-picked-up', {})
-      return
-    }
-
+    // Unused right now
   },
 })
