7dec85b20d9e468fe584fdfe047d5454a6485019
Matt <Geczy@users.noreply.github.com>
2023-01-17 20:35:12 -0600

    fix: if no match id found just err out, or if its local lobby
    also calls the cb() if the hero ids are already found

--- web/src/steam/index.ts
+++ web/src/steam/index.ts
@@ -236,6 +236,10 @@ class Dota {
       )
         .then(async (response) => {
           const game = response.data as delayedGames | undefined
+          if (game?.match?.match_id === '0' || !game?.match?.match_id) {
+            return cb?.(new Error('Match not found'), null)
+          }
+
           const hasTeams = Array.isArray(game?.teams) && game?.teams.length === 2
           const hasPlayers =
             hasTeams &&
@@ -282,6 +286,7 @@ class Dota {
             const players = getAccountsFromMatch(delayedData)
             events.emit('saveHeroesForMatchId', { matchId: game.match.match_id, players }, token)
 
+            cb?.(null, game)
             return
           }
 
