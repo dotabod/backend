869b2c1b6427438c96dc4e15c7578db15b1151a4
Matt <Geczy@users.noreply.github.com>
2023-01-24 19:18:39 -0600

    cmd: add alias to !modsonly !modsonlyoff !modsonlyon, and enable emoteonly

--- web/src/twitch/commands/modsonly.ts
+++ web/src/twitch/commands/modsonly.ts
@@ -6,6 +6,7 @@ import { chatClient } from '../index.js'
 import commandHandler, { MessageType } from '../lib/CommandHandler.js'
 
 commandHandler.registerCommand('modsonly', {
+  aliases: ['modsonlyoff', 'modsonlyon'],
   permission: 2,
   cooldown: 0,
   dbkey: DBSettings.commandModsonly,
@@ -14,15 +15,17 @@ commandHandler.registerCommand('modsonly', {
       channel: { name: channel, id: channelId, client },
     } = message
     if (modMode.has(channelId)) {
-      void chatClient.say(channel, t('modsOnly', { context: 'off', lng: client.locale }))
-      void chatClient.say(channel, '/subscribersoff')
       modMode.delete(channelId)
+      void chatClient.say(channel, t('modsOnly', { context: 'off', lng: client.locale }))
+      void chatClient.say(channel, '/emoteonlyoff')
+      void chatClient.say(channel, '/subscribersoff')
       return
     }
 
     // Delete all messages that are not from a mod
     modMode.add(channelId)
     void chatClient.say(channel, '/subscribers')
+    void chatClient.say(channel, '/emoteonly')
     void chatClient.say(channel, t('modsOnly', { context: 'on', lng: client.locale }))
   },
 })
