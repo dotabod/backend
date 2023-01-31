fad23202d25146eda9a7d858c250353311ea10af
Matt <Geczy@users.noreply.github.com>
2023-01-31 10:28:29 -0600

    github: fix parser

--- web/i18next-parser.config.js
+++ web/i18next-parser.config.js
@@ -39,7 +39,7 @@ export default {
   // Namespace separator used in your translation keys
   // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-  output: 'locales/$LOCALE/$NAMESPACE.json',
+  output: 'web/locales/$LOCALE/$NAMESPACE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
@@ -48,7 +48,7 @@ export default {
   // Plural separator used in your translation keys
   // If you want to use plain english keys, separators such as `_` might conflict. You might want to set `pluralSeparator` to a different string that does not occur in your keys.
 
-  input: undefined,
+  input: 'src/**/*.ts',
   // An array of globs that describe where to look for source files
   // relative to the location of the configuration file
 
