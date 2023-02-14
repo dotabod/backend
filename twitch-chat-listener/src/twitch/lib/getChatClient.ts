9c39ed2d1db6ed44ffb41282870b61960488fce9
Matt <Geczy@users.noreply.github.com>
2023-02-14 09:31:41 -0600

    pog: bot level is now verified

--- twitch-chat-listener/src/twitch/lib/getChatClient.ts
+++ twitch-chat-listener/src/twitch/lib/getChatClient.ts
@@ -6,6 +6,7 @@ import { getChannels } from './getChannels.js'
 export async function getChatClient() {
   const chatClient = new ChatClient({
     isAlwaysMod: true,
+    botLevel: process.env.NODE_ENV === 'production' ? 'verified' : undefined,
     authProvider: await getBotAuthProvider(),
     channels: getChannels,
     webSocket: true,
