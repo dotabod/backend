dc60bf82dc16c6d2a33cd99951f096fd625d3e94
Matt <Geczy@users.noreply.github.com>
2023-02-17 18:16:42 -0600

    fix: no tp msg

--- web/src/dota/events/gsi-events/item.teleport0.ts
+++ web/src/dota/events/gsi-events/item.teleport0.ts
@@ -5,6 +5,7 @@ import { GSIHandler } from '../../GSIHandler.js'
 import { isPlayingMatch } from '../../lib/isPlayingMatch.js'
 import eventHandler from '../EventHandler.js'
 
+// todo: make sure streamer has gold to buy tp?
 eventHandler.registerEvent(`items:teleport0:name`, {
   handler: (dotaClient: GSIHandler, itemName: 'item_tpscroll' | 'empty') => {
     if (!dotaClient.client.beta_tester) return
@@ -29,31 +30,14 @@ eventHandler.registerEvent(`items:teleport0:name`, {
     }
 
     // we found a tp scroll
-    if (itemName !== 'empty') {
-      if (dotaClient.noTpChatter.timeout) {
-        return resetTimer()
-      }
+    if (itemName !== 'empty' && dotaClient.noTpChatter.lastRemindedDate) {
+      const timeSinceLastReminder =
+        (Date.now() - dotaClient.noTpChatter.lastRemindedDate.getTime()) / 1000
+      const seconds = Math.round(timeSinceLastReminder) + secondsToWait
 
-      if (dotaClient.noTpChatter.lastRemindedDate) {
-        const timeSinceLastReminder =
-          (Date.now() - dotaClient.noTpChatter.lastRemindedDate.getTime()) / 1000
-        const seconds = Math.round(timeSinceLastReminder) + secondsToWait
-
-        if (dotaClient.client.gsi?.hero?.alive === false) {
-          dotaClient.say(
-            t('chatters.tpFromDeath', {
-              emote: 'SuskaygeAgreeGe',
-              seconds,
-              channel: `@${dotaClient.client.name}`,
-              lng: dotaClient.client.locale,
-            }),
-            { beta: true },
-          )
-          return resetTimer()
-        }
-
+      if (dotaClient.client.gsi?.hero?.alive === false) {
         dotaClient.say(
-          t('chatters.tpFound', {
+          t('chatters.tpFromDeath', {
             emote: 'SuskaygeAgreeGe',
             seconds,
             channel: `@${dotaClient.client.name}`,
@@ -61,13 +45,27 @@ eventHandler.registerEvent(`items:teleport0:name`, {
           }),
           { beta: true },
         )
-
         return resetTimer()
       }
+
+      dotaClient.say(
+        t('chatters.tpFound', {
+          emote: 'SuskaygeAgreeGe',
+          seconds,
+          channel: `@${dotaClient.client.name}`,
+          lng: dotaClient.client.locale,
+        }),
+        { beta: true },
+      )
+
+      return resetTimer()
     }
 
-    // make sure streamer has gold to buy tp
-    if (itemName === 'empty') {
+    if (
+      itemName === 'empty' &&
+      !dotaClient.noTpChatter.timeout &&
+      !dotaClient.noTpChatter.lastRemindedDate
+    ) {
       dotaClient.noTpChatter.timeout = setTimeout(() => {
         dotaClient.noTpChatter.lastRemindedDate = new Date()
 
