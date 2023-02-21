77b6986eed8762fd5333b57ed0405337ac157b2f
Matt <Geczy@users.noreply.github.com>
2023-02-21 13:04:36 -0600

    fix: dynamic loader grab the file better
    was erroring on dist | src previously

--- packages/dota/src/dota/events/gsiEventLoader.ts
+++ packages/dota/src/dota/events/gsiEventLoader.ts
@@ -1,10 +1,14 @@
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 type GSIEventsMap = Record<string, any>
 const gsiEvents: GSIEventsMap = {}
 
-fs.readdirSync(path.resolve('src', 'dota', 'events', 'gsi-events')).forEach((file) => {
+fs.readdirSync(path.resolve(__dirname, 'gsi-events')).forEach((file) => {
   if (file.endsWith('.js') || file.endsWith('.ts')) {
     const gsiEvent = import(`./gsi-events/${file}`)
     gsiEvents[file.slice(0, -3)] = gsiEvent
--- packages/dota/src/twitch/commandLoader.ts
+++ packages/dota/src/twitch/commandLoader.ts
@@ -1,10 +1,14 @@
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 type CommandMap = Record<string, any>
 const commands: CommandMap = {}
 
-fs.readdirSync(path.resolve('src', 'twitch', 'commands')).forEach((file) => {
+fs.readdirSync(path.resolve(__dirname, 'commands')).forEach((file) => {
   if (file.endsWith('.js') || file.endsWith('.ts')) {
     const command = import(`./commands/${file}`)
     commands[file.slice(0, -3)] = command
