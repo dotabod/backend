a550af3eec8d3c23097137f9f45621a48cc58e3b
Matt <Geczy@users.noreply.github.com>
2023-03-02 21:33:24 -0600

    dev: slight refactor

--- packages/dota/src/dota/index.ts
+++ packages/dota/src/dota/index.ts
@@ -11,59 +11,80 @@ import SetupSupabase from '../db/watcher.js'
 import { logger } from '../utils/logger.js'
 import GSIServer from './GSIServer.js'
 
-logger.info('Starting on', { env: process.env.NODE_ENV })
-
-await i18next.use(FsBackend).init<FsBackendOptions>({
-  initImmediate: false,
-  lng: 'en',
-  fallbackLng: 'en',
-  returnEmptyString: false,
-  returnNull: false,
-  preload: readdirSync(join('./locales')).filter((fileName: string) => {
-    const joinedPath = join(join('./locales'), fileName)
-    const isDirectory = lstatSync(joinedPath).isDirectory()
-    return !!isDirectory
-  }),
-  defaultNS: 'translation',
-  backend: {
-    loadPath: join('./locales/{{lng}}/{{ns}}.json'),
-  },
-})
-
-chokidar
-  .watch('/app/packages/dota/locales/**/*.json', {
-    ignoreInitial: true,
-    usePolling: true,
-    interval: 5000,
-  })
-  .on('all', (_event, filePath) => {
-    const parsedPath = path.parse(filePath)
-    const ns = parsedPath.name
-    const lng = path.basename(parsedPath.dir)
-    i18next
-      .reloadResources([lng], [ns])
-      .then(() => {
-        logger.info(`Translation reloaded`, { filePath })
-      })
-      .catch((error) => {
-        logger.info(`Translation error on reloading`, { error })
-      })
+const setupTranslations = async () => {
+  await i18next.use(FsBackend).init<FsBackendOptions>({
+    initImmediate: false,
+    lng: 'en',
+    fallbackLng: 'en',
+    returnEmptyString: false,
+    returnNull: false,
+    preload: readdirSync(join('./locales')).filter((fileName: string) => {
+      const joinedPath = join(join('./locales'), fileName)
+      const isDirectory = lstatSync(joinedPath).isDirectory()
+      return !!isDirectory
+    }),
+    defaultNS: 'translation',
+    backend: {
+      loadPath: join('./locales/{{lng}}/{{ns}}.json'),
+    },
   })
 
-logger.info('Loaded translations')
+  chokidar
+    .watch('/app/packages/dota/locales/**/*.json', {
+      ignoreInitial: true,
+      usePolling: true,
+      interval: 5000,
+    })
+    .on('all', (_event, filePath) => {
+      console.log({ _event, filePath })
+      const parsedPath = path.parse(filePath)
+      const ns = parsedPath.name
+      const lng = path.basename(parsedPath.dir)
+      i18next
+        .reloadResources([lng], [ns])
+        .then(() => {
+          logger.info(`Translation reloaded`, { filePath })
+        })
+        .catch((error) => {
+          logger.info(`Translation error on reloading`, { error })
+        })
+    })
 
-// Set up the supabase watcher
-const supabaseWatcher = new SetupSupabase()
-supabaseWatcher.init()
-logger.info('Supabase watcher started')
+  logger.info('Loaded translations')
+}
 
-// Here's where we force wait for the redis to connect before starting the server
-const redisClient = RedisClient.getInstance()
-logger.info('Redis client created')
-await redisClient.connectClient()
-logger.info('Redis client connected')
-await redisClient.connectSubscriber()
-logger.info('Redis subscriber connected')
+const setupSupabaseWatcher = () => {
+  const supabaseWatcher = new SetupSupabase()
+  supabaseWatcher.init()
+  logger.info('Supabase watcher started')
+}
 
-// Then set up the dota gsi server & websocket server
-export const server = new GSIServer()
+const setupRedisClient = async () => {
+  const redisClient = RedisClient.getInstance()
+  logger.info('Redis client created')
+  await redisClient.connectClient()
+  logger.info('Redis client connected')
+  await redisClient.connectSubscriber()
+  logger.info('Redis subscriber connected')
+}
+
+const startServer = () => {
+  const server = new GSIServer()
+  server.init()
+  logger.info('GSIServer started')
+
+  return server
+}
+
+const main = async () => {
+  logger.info('Starting on', { env: process.env.NODE_ENV })
+
+  await setupRedisClient()
+  await setupTranslations()
+  setupSupabaseWatcher()
+
+  return startServer()
+}
+
+const server = await main()
+export default server
