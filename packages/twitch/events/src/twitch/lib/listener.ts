6c4e842126818f1b5d0425da71c6fa8ef5c8d96e
Matt <Geczy@users.noreply.github.com>
2023-03-14 14:09:28 -0500

    twurple v6

--- packages/twitch/events/src/twitch/lib/listener.ts
+++ packages/twitch/events/src/twitch/lib/listener.ts
@@ -4,9 +4,9 @@ import BotAPI from './BotApiSingleton.js'
 const botApi = BotAPI.getInstance()
 
 // TODO: Remove this next time you push to production
-// waiting for v6 to be released
+// waiting for v6.1 to be released
 // console.log('delete all v5 subs, only should run once')
-await botApi.eventSub.deleteAllSubscriptions()
+// await botApi.eventSub.deleteAllSubscriptions()
 
 console.log('Create the event sub listener')
 const listener = new EventSubHttpListener({
