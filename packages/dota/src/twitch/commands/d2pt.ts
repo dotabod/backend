b4202df762232e6645ada95c0b9d1042f6dcaad1
Matt Gates <Geczy@users.noreply.github.com>
2023-07-18 09:04:30 +0200

    fix: !d2pt command to return url for a build
    removes the dev server for now

--- packages/dota/src/twitch/commands/d2pt.ts
+++ packages/dota/src/twitch/commands/d2pt.ts
@@ -1,5 +1,4 @@
 import { DBSettings } from '@dotabod/settings'
-import axios from 'axios'
 import { t } from 'i18next'
 
 import { gsiHandlers } from '../../dota/lib/consts.js'
@@ -43,10 +42,6 @@ commandHandler.registerCommand('d2pt', {
     const spectatorPlayers = getCurrentMatchPlayers(client.gsi)
     const myPlayers = gsiHandlers.get(client.token)?.players?.matchPlayers
     const selectedPlayer = spectatorPlayers.find((a) => a.selected)
-    const selectedPlayerIdx = spectatorPlayers.findIndex((a) => a.selected)
-
-    // if its 0-4 its radiant, if its 5-9 its dire
-    const isRadiant = selectedPlayerIdx < 5
 
     if (!hero) {
       if (!selectedPlayer && !myPlayers) {
@@ -55,39 +50,15 @@ commandHandler.registerCommand('d2pt', {
       }
     }
 
-    const [position] = args
-    if (!args.length || !Number(position) || Number(position) > 5 || Number(position) < 1) {
-      chatClient.say(channel, 'missing position, try !d2pt 1-5, eg !d2pt 2 for mid')
-      return
-    }
-
-    // take first 5 object keys only if radiant, or last 5 if dire
-    const players = (
-      gsiHandlers.get(client.token)?.players?.matchPlayers || getCurrentMatchPlayers(client.gsi)
-    ).slice(isRadiant ? 5 : 0, !isRadiant ? 10 : 5)
-
-    const draft = players.map((a) => a.heroid).join(',') || '' // list of hero ids on enemy team
-    const heroId = `${hero ? hero.id : selectedPlayer?.heroid ?? 0}`
-    const url = `http://65.108.208.220:8888/itemizer?heroId=${heroId}&position=${position}&draft=${draft}&token=${process
-      .env.D2PT_TOKEN!}`
-
     const heroName = hero ? hero.localized_name : getHeroNameById(selectedPlayer?.heroid ?? 0)
 
-    axios
-      .get(url)
-      .then((response) => {
-        chatClient.say(channel, response.data?.items.join(', ') || 'No items found')
-        chatClient.say(
-          channel,
-          t('dota2pt', {
-            heroName,
-            url: `dota2protracker.com/hero/${encodeURI(heroName).replace(/'/g, '%27')}`,
-            lng: message.channel.client.locale,
-          }),
-        )
-      })
-      .catch((e) => {
-        console.log(e?.data, e?.response?.data)
-      })
+    chatClient.say(
+      channel,
+      t('dota2pt', {
+        heroName,
+        url: `dota2protracker.com/hero/${encodeURI(heroName).replace(/'/g, '%27')}`,
+        lng: message.channel.client.locale,
+      }),
+    )
   },
 })
