c07457a809fe664bd956b08370d01bd81d162cc4
Matt Gates <Geczy@users.noreply.github.com>
2023-07-31 21:46:14 -0500

    fix: dont try to sub to those that haven't refreshed

--- packages/twitch/events/src/twitch/lib/getAccountIds.ts
+++ packages/twitch/events/src/twitch/lib/getAccountIds.ts
@@ -14,9 +14,6 @@ export async function getAccountIds() {
       select: { providerAccountId: true },
       where: {
         NOT: {
-          providerAccountId: {
-            in: dev_channelids,
-          },
           requires_refresh: true,
         },
       },
