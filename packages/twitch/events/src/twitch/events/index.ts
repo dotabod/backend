3f733fe231ad540e68fb37e9febc200c9d55d1cc
Matt Gates <Geczy@users.noreply.github.com>
2023-07-31 22:00:33 -0500

    fix: get socket

--- packages/twitch/events/src/twitch/events/index.ts
+++ packages/twitch/events/src/twitch/events/index.ts
@@ -7,6 +7,7 @@ export let eventsIOConnected = false
 io.on('connection', (socket) => {
   try {
     void socket.join(DOTABOD_EVENTS_ROOM)
+    console.log('Joined socket DOTABOD_EVENTS_ROOM')
   } catch (e) {
     console.error('could not join socket DOTABOD_EVENTS_ROOM', e)
     return
