05d7b45524c9efd411df1392012feb5499e64728
Matt Gates <Geczy@users.noreply.github.com>
2023-08-06 09:58:24 -0400

    refactor: getWL

--- packages/dota/src/db/getWL.ts
+++ packages/dota/src/db/getWL.ts
@@ -10,6 +10,39 @@ interface WL {
   startDate?: Date | null
 }
 
+const LOBBY_TYPE_RANKED = 7
+const MULTIPLIER_PARTY = 20
+const MULTIPLIER_SOLO = 25
+
+const updateStats = (
+  stats: {
+    win: number
+    lose: number
+    mmr?: number
+  },
+  match: {
+    won: boolean | null
+    is_doubledown: boolean
+    _count: {
+      won: number
+      is_party: number
+      is_doubledown: number
+    }
+  },
+  multiplier: number,
+) => {
+  if (match.won) {
+    stats.win += match._count.won
+  } else {
+    stats.lose += match._count.won
+  }
+
+  if (stats.mmr !== undefined) {
+    const wonMulti = match._count.won * (match.won ? 1 : -1)
+    stats.mmr += wonMulti * (match.is_doubledown ? multiplier * 2 : multiplier)
+  }
+}
+
 export async function getWL({ lng, channelId, mmrEnabled, startDate }: WL) {
   if (!channelId) {
     return Promise.resolve({ record: [{ win: 0, lose: 0, type: 'U' }], msg: null })
@@ -29,7 +62,7 @@ export async function getWL({ lng, channelId, mmrEnabled, startDate }: WL) {
         },
         lobby_type: {
           not: null,
-          in: [0, 7],
+          in: [0, LOBBY_TYPE_RANKED],
         },
         user: {
           Account: {
@@ -38,8 +71,6 @@ export async function getWL({ lng, channelId, mmrEnabled, startDate }: WL) {
           },
         },
         createdAt: {
-          // if its null then only the last 12 hours from now
-          // should never be null but just in case
           gte: startDate ?? new Date(new Date().getTime() - 12 * 60 * 60 * 1000),
         },
       },
@@ -49,42 +80,35 @@ export async function getWL({ lng, channelId, mmrEnabled, startDate }: WL) {
       const unranked: { win: number; lose: number } = { win: 0, lose: 0 }
 
       matches.forEach((match) => {
-        if (match.lobby_type === 7) {
-          if (match.won) {
-            ranked.win += match._count.won
-          } else {
-            ranked.lose += match._count.won
-          }
+        const isRanked = match.lobby_type === LOBBY_TYPE_RANKED
+        const stats = isRanked ? ranked : unranked
+        const multiplier = isRanked ? (match.is_party ? MULTIPLIER_PARTY : MULTIPLIER_SOLO) : 0
 
-          const wonMulti = match._count.won * (match.won ? 1 : -1)
-          const multiplier = match.is_party ? 20 : 25
-          ranked.mmr += wonMulti * (match.is_doubledown ? multiplier * 2 : multiplier)
-        } else {
-          if (match.won) {
-            unranked.win += match._count.won
-          } else {
-            unranked.lose += match._count.won
-          }
-        }
+        updateStats(stats, match, multiplier)
       })
 
       const hasUnranked = unranked.win + unranked.lose !== 0
       const hasRanked = ranked.win + ranked.lose !== 0
 
-      const record = []
-      if (hasRanked) record.push({ win: ranked.win, lose: ranked.lose, type: 'R' })
-      if (hasUnranked) record.push({ win: unranked.win, lose: unranked.lose, type: 'U' })
-      if (!hasRanked && !hasUnranked) record.push({ win: 0, lose: 0, type: 'U' })
+      const record = [
+        hasRanked ? { win: ranked.win, lose: ranked.lose, type: 'R' } : null,
+        hasUnranked ? { win: unranked.win, lose: unranked.lose, type: 'U' } : null,
+        !hasRanked && !hasUnranked ? { win: 0, lose: 0, type: 'U' } : null,
+      ].filter(Boolean)
 
-      const msg = []
       const mmrMsg = mmrEnabled ? ` | ${ranked.mmr >= 0 ? '+' : ''}${ranked.mmr} MMR` : ''
       const rankedMsg = `${t('ranked', { lng })} ${ranked.win} W - ${ranked.lose} L${mmrMsg}`
       const unrankedMsg = `${t('unranked', { lng })} ${unranked.win} W - ${unranked.lose} L`
 
-      if (hasRanked) msg.push(rankedMsg)
-      if (hasUnranked) msg.push(unrankedMsg)
-      if (!hasRanked && !hasUnranked) msg.push('0 W - 0 L')
-      return { record, msg: msg.join(' · ') }
+      const msg = [
+        hasRanked ? rankedMsg : null,
+        hasUnranked ? unrankedMsg : null,
+        !hasRanked && !hasUnranked ? '0 W - 0 L' : null,
+      ]
+        .filter(Boolean)
+        .join(' · ')
+
+      return { record, msg }
     })
     .catch((e) => {
       logger.error('[WL] Prisma Error getting WL', { error: e.message, channelId })
