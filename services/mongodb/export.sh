bc78e36795e3ac9d96bbb12d13e899f2205f5658
Matt Gates <Geczy@users.noreply.github.com>
2023-08-16 15:27:12 -0500

    Update export.sh
    show the node env

--- services/mongodb/export.sh
+++ services/mongodb/export.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo "Exporting data from MongoDB to exported_data.json"
+echo "$NODE_ENV"
+
 mongoexport --jsonArray --uri "$MONGO_URL" --collection notablePlayers --type json --out ./exported_data.json
