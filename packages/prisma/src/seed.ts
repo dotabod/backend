da103d2bb5847c66c80f339ab250a09cbdf590f5
Matt Gates <Geczy@users.noreply.github.com>
2023-08-29 21:01:40 -0500

    dev: add a database seeder

--- packages/prisma/src/seed.ts
+++ packages/prisma/src/seed.ts
@@ -3,15 +3,32 @@ import { PrismaClient as PrismaPsql } from "@dotabod/prisma/dist/psql/index.js"
 
 const prisma = new PrismaPsql()
 
+const usedProviderAccountIds = new Set()
+
+function generateUniqueProviderAccountId(): string {
+  let id
+  do {
+    id = faker.number.int({ min: 50_000, max: 900_000 })
+  } while (usedProviderAccountIds.has(id))
+  usedProviderAccountIds.add(id)
+  return id.toString()
+}
+
 async function main() {
   for (let i = 0; i < 100; i++) {
     // Seed User data
+    const username = faker.internet.userName()
     const user = await prisma.user.create({
       data: {
-        displayName: faker.person.fullName(),
+        stream_online: faker.datatype.boolean({ probability: 0.75 }),
+        beta_tester: faker.datatype.boolean({ probability: 0.1 }),
+        stream_start_date: faker.date.recent(),
+        locale: faker.helpers.arrayElement(["en", "ru-RU"]),
         email: faker.internet.email(),
-        mmr: faker.number.int({ min: 0, max: 2000 }),
-        name: faker.internet.userName(),
+        mmr: faker.number.int({ min: 0, max: 10_000 }),
+        followers: faker.number.int({ min: 0, max: 800_000 }),
+        name: username.toLowerCase(),
+        displayName: username,
       },
     })
 
@@ -19,11 +36,17 @@ async function main() {
     await prisma.account.create({
       data: {
         userId: user.id,
-        type: "basic",
+        type: "oauth",
         provider: "twitch",
-        providerAccountId: faker.string.uuid(),
+        token_type: "bearer",
+        scope:
+          "channel:manage:polls channel:manage:predictions channel:read:polls channel:read:predictions openid user:read:email",
+        providerAccountId: generateUniqueProviderAccountId(),
         refresh_token: faker.string.alphanumeric(30),
         access_token: faker.string.alphanumeric(30),
+        expires_at: faker.date.future().getTime(),
+        expires_in: faker.number.int({ min: 1000, max: 10000 }),
+        requires_refresh: faker.datatype.boolean({ probability: 0.1 }),
       },
     })
 
@@ -36,43 +59,48 @@ async function main() {
       },
     })
 
+    const steam32Id = Number(generateUniqueProviderAccountId())
+
     // Seed SteamAccount data
     await prisma.steamAccount.create({
       data: {
-        steam32Id: faker.number.int({ min: 50_000, max: 70_000 }),
+        steam32Id: steam32Id,
         userId: user.id,
         name: faker.internet.userName(),
+        mmr: faker.number.int({ min: 0, max: 10_000 }),
+        leaderboard_rank: faker.number.int({ min: 1, max: 5000 }),
       },
     })
 
-    // Seed Bet data
-    await prisma.bet.create({
-      data: {
-        matchId: faker.string.uuid(),
-        predictionId: faker.string.uuid(),
-        userId: user.id,
-        myTeam: faker.helpers.arrayElement(["Radiant", "Dire"]),
-      },
-    })
-
-    // Seed mods data
-    await prisma.mods.create({
-      data: {
-        streamer_user_id: user.id,
-        mod_user_id: null,
-        temp_mod_name: faker.person.fullName(),
-      },
-    })
-
-    // Seed streams data
-    await prisma.streams.create({
-      data: {
-        id: faker.string.uuid(),
-        userId: user.id,
-        category_name: faker.helpers.arrayElement(["Gaming", "Education", "Cooking"]),
-        title: faker.company.catchPhrase(),
-      },
-    })
+    for (let i = 0; i < faker.number.int(50); i++) {
+      // Seed Bet data
+      await prisma.bet.create({
+        data: {
+          matchId: `${faker.number.int({ min: 700_000, max: 1_000_000 })}`,
+          predictionId: faker.string.uuid(),
+          userId: user.id,
+          won: faker.datatype.boolean({ probability: 0.5 }),
+          myTeam: faker.helpers.arrayElement(["dire", "radiant"]),
+          lobby_type: faker.helpers.arrayElement([0, 7]),
+          steam32Id: steam32Id,
+          is_party: faker.datatype.boolean({ probability: 0.1 }),
+          hero_slot: faker.number.int({ min: 0, max: 9 }),
+          is_doubledown: faker.datatype.boolean({ probability: 0.1 }),
+          radiant_score: faker.number.int({ min: 0, max: 50 }),
+          dire_score: faker.number.int({ min: 0, max: 50 }),
+          kda: {
+            kills: faker.number.int({ min: 0, max: 50 }),
+            deaths: faker.number.int({ min: 0, max: 50 }),
+            assists: faker.number.int({ min: 0, max: 50 }),
+          },
+          hero_name: faker.helpers.arrayElement([
+            "npc_dota_hero_antimage",
+            "npc_dota_hero_axe",
+            "npc_dota_hero_bane",
+          ]),
+        },
+      })
+    }
   }
 
   console.log("Seeding finished.")
