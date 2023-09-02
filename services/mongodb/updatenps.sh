51d37f2e3bcc970fadcb0e881f6d1fc2c3d66fd5
Matt Gates <Geczy@users.noreply.github.com>
2023-09-02 13:04:49 -0500

    fix line endings

--- services/mongodb/updatenps.sh
+++ services/mongodb/updatenps.sh
@@ -2,5 +2,6 @@
 # Description: Updates notable players from OpenDota API
 
 wget https://api.opendota.com/api/proPlayers -O full.json
-mongoimport --jsonArray --uri "$MONGO_URL" --collection notablePlayers --type json --mode upsert --upsertFields=account_id,channel --file ./full.json
+mongoimport --jsonArray --uri "$MONGO_URL" --collection notablePlayers --type json --mode upsert --upsertFields=account_id,channel --file "full.json"
+
 rm full.json
