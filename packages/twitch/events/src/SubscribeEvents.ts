0634dce389a9f1e4e2ad30320bf1574295e4cf35
Matt <Geczy@users.noreply.github.com>
2023-09-20 16:12:49 -0500

    stop trying to apply ts and loops lol, just do it directly
    was causing more issues than i want to admit, spent like 3h on this last night just to get the ts working xd

--- packages/twitch/events/src/SubscribeEvents.ts
+++ packages/twitch/events/src/SubscribeEvents.ts
@@ -1,23 +1,63 @@
 import { middleware } from './listener.js'
-import { events } from './twitch/events/events.js'
-import { handleEvent } from './twitch/events/handleEvent.js'
+import { transformBetData } from './twitch/events/transformers/transformBetData.js'
+import { transformPollData } from './twitch/events/transformers/transformPollData.js'
+import { offlineEvent } from './twitch/lib/offlineEvent.js'
+import { onlineEvent } from './twitch/lib/onlineEvent.js'
+import { updateUserEvent } from './twitch/lib/updateUserEvent.js'
+import { DOTABOD_EVENTS_ROOM, eventsIOConnected, socketIo } from './utils/socketUtils.js'
+
+export const handleEvent = (eventName: any, data: any) => {
+  if (!eventsIOConnected) return
+  socketIo.to(DOTABOD_EVENTS_ROOM).emit('event', eventName, data.broadcasterId, data)
+}
 
 export const SubscribeEvents = (accountIds: string[]) => {
-  const promises: Promise<void>[] = []
+  const promises: any[] = []
   accountIds.forEach((userId) => {
     try {
+      promises.push(middleware.onStreamOnline(userId, onlineEvent))
+      promises.push(middleware.onStreamOffline(userId, offlineEvent))
+      promises.push(middleware.onUserUpdate(userId, updateUserEvent))
       promises.push(
-        ...Object.keys(events).map((eventName) => {
-          const eventNameTyped = eventName as keyof typeof events
-          try {
-            const middlewareEvent = middleware[eventNameTyped]
-            // @ts-expect-error asdf
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            return middlewareEvent(userId, (data) => handleEvent(eventNameTyped, data))
-          } catch (error) {
-            console.error('[TWITCHEVENTS] Could not sub userId error', { userId, error })
-          }
-        }),
+        middleware.onChannelPredictionBegin(userId, (data) =>
+          handleEvent('onChannelPredictionBegin', transformBetData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPredictionProgress(userId, (data) =>
+          handleEvent('onChannelPredictionProgress', transformBetData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPredictionLock(userId, (data) =>
+          handleEvent('onChannelPredictionLock', transformBetData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPredictionEnd(userId, (data) =>
+          handleEvent('onChannelPredictionEnd', transformBetData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPollBegin(userId, (data) =>
+          handleEvent('onChannelPollBegin', transformPollData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPollProgress(userId, (data) =>
+          handleEvent('onChannelPollProgress', transformPollData(data)),
+        ),
+      )
+      promises.push(
+        middleware.onChannelPollEnd(userId, (data) =>
+          handleEvent('onChannelPollEnd', transformPollData(data)),
+        ),
+      )
+
+      promises.push(
+        middleware.onChannelPredictionBegin(userId, (data) =>
+          handleEvent('onChannelPredictionBegin', data),
+        ),
       )
     } catch (e) {
       console.log('[TWITCHEVENTS] could not sub', { e, userId })
