63affa37068c241687c8029c482fcd883923b27c
Matt <Geczy@users.noreply.github.com>
2023-09-20 19:06:28 -0500

    dev: yarn v2 build

--- packages/twitch/chat/src/twitch/lib/getAuthProvider.ts
+++ packages/twitch/chat/src/twitch/lib/getAuthProvider.ts
@@ -12,28 +12,27 @@ export const getAuthProvider = function () {
   authProvider = new RefreshingAuthProvider({
     clientId: process.env.TWITCH_CLIENT_ID ?? '',
     clientSecret: process.env.TWITCH_CLIENT_SECRET ?? '',
-    onRefresh: (twitchId, newTokenData) => {
-      console.log('[TWITCHSETUP] Refreshing twitch tokens', { twitchId })
+  })
 
-      void supabase
-        .from('accounts')
-        .update({
-          scope: newTokenData.scope.join(' '),
-          access_token: newTokenData.accessToken,
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          refresh_token: newTokenData.refreshToken!,
-          expires_at: Math.floor(
-            new Date(newTokenData.obtainmentTimestamp).getTime() / 1000 +
-              (newTokenData.expiresIn ?? 0),
-          ),
-          expires_in: newTokenData.expiresIn ?? 0,
-          obtainment_timestamp: new Date(newTokenData.obtainmentTimestamp).toISOString(),
-        })
-        .eq('providerAccountId', twitchId)
-        .then(() => {
-          console.log('[TWITCHSETUP] Updated bot tokens', { twitchId })
-        })
-    },
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  authProvider.onRefresh(async (twitchId, newTokenData) => {
+    console.log('[TWITCHSETUP] Refreshing twitch tokens', { twitchId })
+
+    await supabase
+      .from('accounts')
+      .update({
+        scope: newTokenData.scope.join(' '),
+        access_token: newTokenData.accessToken,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        refresh_token: newTokenData.refreshToken!,
+        expires_at: Math.floor(
+          new Date(newTokenData.obtainmentTimestamp).getTime() / 1000 +
+            (newTokenData.expiresIn ?? 0),
+        ),
+        expires_in: newTokenData.expiresIn ?? 0,
+        obtainment_timestamp: new Date(newTokenData.obtainmentTimestamp).toISOString(),
+      })
+      .eq('providerAccountId', twitchId)
   })
 
   return authProvider
