d65b312b2b9790f9a8466d9f6ca274fcd7b5ddad
Matt Gates <Geczy@users.noreply.github.com>
2023-09-29 11:48:39 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -115,13 +115,13 @@ restart() {
 
 pull() {
     docker_login
-    docker_command pull twitch-events dota twitch-chat steam
+    docker_command pull
 }
 
 push() {
     docker_login
     buildall
-    docker_command push twitch-events dota twitch-chat steam
+    docker_command push
 }
 
 buildall() {
