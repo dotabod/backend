0002f3ddf69682c67edc1b75d60cc7ac631551e0
Matt <Geczy@users.noreply.github.com>
2024-01-14 02:44:59 -0600

    merge

--- packages/dota/src/dota/events/gsi-events/newdata.ts
+++ packages/dota/src/dota/events/gsi-events/newdata.ts
@@ -115,6 +115,39 @@ async function saveMatchData(client: SocketClient) {
   }
 }
 
+const tooltipsConfig = {
+  clientId: process.env.TWITCH_EXT_CLIENT_ID!,
+  secret: process.env.TWITCH_EXT_SECRET!,
+  ownerId: process.env.TWITCH_BOT_PROVIDERID!,
+}
+
+const sendExtensionPubSubBroadcastMessageIfChanged = async (
+  dotaClient: GSIHandler,
+  messageToSend: any,
+) => {
+  const { client } = dotaClient
+  const matchId = await redisClient.client.get(`${client.token}:matchId`)
+  if (!Number(matchId)) return
+
+  const redisKey = `${client.token}:matchId:${matchId}:lastMessage`
+
+  // Retrieve the previous message from Redis
+  const prevMessageString = await redisClient.client.get(redisKey)
+
+  // Convert the current message to a string for comparison
+  const currentMessageString = JSON.stringify(messageToSend)
+
+  // Compare the current message with the previous one
+  if (currentMessageString !== prevMessageString) {
+    const accountId = client.Account?.providerAccountId ?? ''
+    if (!accountId) return
+
+    // If different, send the message and update Redis
+    await sendExtensionPubSubBroadcastMessage(tooltipsConfig, accountId, currentMessageString)
+    await redisClient.client.set(redisKey, currentMessageString)
+  }
+}
+
 // Catch all
 eventHandler.registerEvent(`newdata`, {
   handler: async (dotaClient: GSIHandler, data: Packet) => {
