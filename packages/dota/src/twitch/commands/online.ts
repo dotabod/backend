24968b7f182f9f1895284c3bb7a18043c6d6d92c
Matt Gates <Geczy@users.noreply.github.com>
2024-06-28 11:11:57 -0400

    dev: refactor !online command, fix toggling

--- packages/dota/src/twitch/commands/online.ts
+++ packages/dota/src/twitch/commands/online.ts
@@ -1,55 +1,66 @@
 import { t } from 'i18next'
-
 import supabase from '../../db/supabase.js'
 import { server } from '../../dota/index.js'
 import { chatClient } from '../chatClient.js'
 import commandHandler from '../lib/CommandHandler.js'
 
 commandHandler.registerCommand('online', {
-  aliases: ['offline', 'forceonline', 'forceoffline'],
+  aliases: ['offline'],
   permission: 2,
   cooldown: 0,
   handler: async (message, args, command) => {
     const {
-      channel: { name: channel, client },
+      channel: { client },
     } = message
 
-    const forceOnline = command === 'forceonline' || command === 'online'
-    if (message.channel.client.stream_online === forceOnline) {
-      chatClient.say(
-        message.channel.name,
-        t('stream', {
-          lng: message.channel.client.locale,
-          channel: message.channel.name,
-          state: message.channel.client.stream_online
-            ? t('online', { lng: message.channel.client.locale })
-            : t('offline', { lng: message.channel.client.locale }),
-          command: message.channel.client.stream_online ? 'offline' : 'online',
-          context: 'none',
-        }),
-      )
+    const isOnlineCommand = command === 'online'
+    const streamOnline = client.stream_online
 
-      server.io.to(client.token).emit('refresh-settings')
+    if ((streamOnline && isOnlineCommand) || (!streamOnline && !isOnlineCommand)) {
+      const state = streamOnline ? 'online' : 'offline'
+      const oppositeCommand = isOnlineCommand ? 'offline' : 'online'
+
+      notifyStreamStatus(message.channel.name, client.locale, state, oppositeCommand)
+      refreshSettings(client.token)
       return
     }
 
-    await supabase
-      .from('users')
-      .update({
-        stream_online: forceOnline,
-        stream_start_date: null,
-      })
-      .eq('id', message.channel.client.token)
+    await updateStreamStatus(client.token, isOnlineCommand)
 
-    server.io.to(client.token).emit('refresh-settings')
-
-    chatClient.say(
-      message.channel.name,
-      t('stream', {
-        lng: message.channel.client.locale,
-        channel: message.channel.name,
-        context: message.channel.client.stream_online ? 'off' : 'on',
-      }),
-    )
+    refreshSettings(client.token)
+    notifyStreamStatus(message.channel.name, client.locale, '', isOnlineCommand ? 'on' : 'off')
   },
 })
+
+const notifyStreamStatus = (
+  channelName: string,
+  locale: string,
+  state: string,
+  command?: string,
+  context = 'none',
+) => {
+  chatClient.say(
+    channelName,
+    t('stream', {
+      lng: locale,
+      channel: channelName,
+      state: t(state, { lng: locale }),
+      command,
+      context,
+    }),
+  )
+}
+
+const refreshSettings = (token: string) => {
+  server.io.to(token).emit('refresh-settings')
+}
+
+const updateStreamStatus = async (token: string, isOnline: boolean) => {
+  await supabase
+    .from('users')
+    .update({
+      stream_online: isOnline,
+      stream_start_date: null,
+    })
+    .eq('id', token)
+}
