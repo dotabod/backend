5a1ad9b44534fb2c00515ec6dd7074a3a2d3bbb4
Matt Gates <Geczy@users.noreply.github.com>
2024-11-12 15:48:28 -0600

    fixes to the sql trigger

--- packages/twitch-events/src/refresh_trigger.sql
+++ packages/twitch-events/src/refresh_trigger.sql
@@ -1,70 +1,72 @@
 DECLARE
-      request_id bigint;
-      payload jsonb;
-      url text := TG_ARGV[0]::text;
-      method text := TG_ARGV[1]::text;
-      headers jsonb DEFAULT '{}'::jsonb;
-      params jsonb DEFAULT '{}'::jsonb;
-      timeout_ms integer DEFAULT 1000;
-    BEGIN
-      IF url IS NULL OR url = 'null' THEN
-        RAISE EXCEPTION 'url argument is missing';
-      END IF;
+  request_id bigint;
+  payload jsonb;
+  url text := TG_ARGV[0]::text;
+  method text := TG_ARGV[1]::text;
+  headers jsonb DEFAULT '{}'::jsonb;
+  params jsonb DEFAULT '{}'::jsonb;
+  timeout_ms integer DEFAULT 1000;
+BEGIN
+  IF url IS NULL OR url = 'null' THEN
+    RAISE EXCEPTION 'url argument is missing';
+  END IF;
 
-      IF method IS NULL OR method = 'null' THEN
-        RAISE EXCEPTION 'method argument is missing';
-      END IF;
+  IF method IS NULL OR method = 'null' THEN
+    RAISE EXCEPTION 'method argument is missing';
+  END IF;
 
-      IF TG_ARGV[2] IS NULL OR TG_ARGV[2] = 'null' THEN
-        headers = '{"Content-Type": "application/json"}'::jsonb;
-      ELSE
-        headers = TG_ARGV[2]::jsonb;
-      END IF;
+  IF TG_ARGV[2] IS NULL OR TG_ARGV[2] = 'null' THEN
+    headers = '{"Content-Type": "application/json"}'::jsonb;
+  ELSE
+    headers = TG_ARGV[2]::jsonb;
+  END IF;
 
-      IF TG_ARGV[3] IS NULL OR TG_ARGV[3] = 'null' THEN
-        params = '{}'::jsonb;
-      ELSE
-        params = TG_ARGV[3]::jsonb;
-      END IF;
+  IF TG_ARGV[3] IS NULL OR TG_ARGV[3] = 'null' THEN
+    params = '{}'::jsonb;
+  ELSE
+    params = TG_ARGV[3]::jsonb;
+  END IF;
 
-      IF TG_ARGV[4] IS NULL OR TG_ARGV[4] = 'null' THEN
-        timeout_ms = 1000;
-      ELSE
-        timeout_ms = TG_ARGV[4]::integer;
-      END IF;
+  IF TG_ARGV[4] IS NULL OR TG_ARGV[4] = 'null' THEN
+    timeout_ms = 1000;
+  ELSE
+    timeout_ms = TG_ARGV[4]::integer;
+  END IF;
 
-      -- Check if requires_refresh has changed and if the new value is not true
-      IF (NEW."requires_refresh" IS DISTINCT FROM OLD."requires_refresh") AND (NEW."requires_refresh" IS NOT TRUE) THEN
-        RETURN NEW;
-      END IF;
+  -- Proceed only if requires_refresh has changed and the new value is false
+  IF (NEW."requires_refresh" IS DISTINCT FROM OLD."requires_refresh") AND (NEW."requires_refresh" IS FALSE) THEN
+    -- Continue to perform the HTTP request
+  ELSE
+    RETURN NEW;
+  END IF;
 
-      CASE
-        WHEN method = 'GET' THEN
-          SELECT http_get INTO request_id FROM net.http_get(
-            url,
-            params,
-            headers,
-            timeout_ms
-          );
-        WHEN method = 'POST' THEN
-          payload = jsonb_build_object(
-            'old_record', OLD,
-            'record', NEW,
-            'type', TG_OP,
-            'table', TG_TABLE_NAME,
-            'schema', TG_TABLE_SCHEMA
-          );
+  CASE
+    WHEN method = 'GET' THEN
+      SELECT http_get INTO request_id FROM net.http_get(
+        url,
+        params,
+        headers,
+        timeout_ms
+      );
+    WHEN method = 'POST' THEN
+      payload = jsonb_build_object(
+        'old_record', OLD,
+        'record', NEW,
+        'type', TG_OP,
+        'table', TG_TABLE_NAME,
+        'schema', TG_TABLE_SCHEMA
+      );
 
-          SELECT http_post INTO request_id FROM net.http_post(
-            url,
-            payload,
-            params,
-            headers,
-            timeout_ms
-          );
-        ELSE
-          RAISE EXCEPTION 'method argument % is invalid', method;
-      END CASE;
+      SELECT http_post INTO request_id FROM net.http_post(
+        url,
+        payload,
+        params,
+        headers,
+        timeout_ms
+      );
+    ELSE
+      RAISE EXCEPTION 'method argument % is invalid', method;
+  END CASE;
 
-      RETURN NEW;
-    END
+  RETURN NEW;
+END
