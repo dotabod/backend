f94b098d12ba1a460d87fa312f7ac46343fd665e
Matt Gates <Geczy@users.noreply.github.com>
2025-05-01 20:55:36 +0200

    fix: reintroduce gift subscription check in isSubscriptionActive function
    - Added a check for `subscription?.isGift` in the `isSubscriptionActive` function to ensure that gift subscriptions are not considered active, addressing edge cases where credits may not be applied immediately.

--- packages/dota/src/types/subscription.ts
+++ packages/dota/src/types/subscription.ts
@@ -11,6 +11,11 @@ export const SUBSCRIPTION_TIERS = {
 } as const
 
 export function isSubscriptionActive(subscription?: SubscriptionRow): boolean {
+  // The credits need to be applied to be active
+  // Dotabod creates a subscription for the gift, and the credits are immediately applied
+  // But in rare cases, the subscription is not created in time, or they could not have enough
+  // credits to activate immediately.
+  if (subscription?.isGift) return false
   if (!subscription?.status) return false
   if (subscription.status === 'TRIALING') return true
   if (subscription.status === 'ACTIVE') return true
