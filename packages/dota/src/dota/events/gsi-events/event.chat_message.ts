8fa97033550f83bfcf17817f629e383658beb3fe
Matt <Geczy@users.noreply.github.com>
2025-09-27 18:35:46 -0500

    send chat msg to frontend overlay

--- packages/dota/src/dota/events/gsi-events/event.chat_message.ts
+++ packages/dota/src/dota/events/gsi-events/event.chat_message.ts
@@ -1,14 +1,11 @@
+import { moderateText } from '@dotabod/profanity-filter'
 import { logger } from '@dotabod/shared-utils'
 import { translate } from '@vitalets/google-translate-api'
+import { franc } from 'franc'
 import { DotaEventTypes } from '../../../types.js'
 import { isPlayingMatch } from '../../lib/isPlayingMatch.js'
+import { server } from '../../server.js'
 import eventHandler from '../EventHandler.js'
-
-function isRussian(text: string): boolean {
-  // Check if the text contains Cyrillic characters
-  const cyrillicRegex = /[\u0400-\u04FF]/
-  return cyrillicRegex.test(text)
-}
 
 eventHandler.registerEvent(`event:${DotaEventTypes.ChatMessage}`, {
   handler: async (
@@ -18,23 +15,25 @@ eventHandler.registerEvent(`event:${DotaEventTypes.ChatMessage}`, {
       player_id: number
       channel_type: number
       event_type: string
+      message: string
     },
   ) => {
     if (!dotaClient.client.stream_online) return
     if (!isPlayingMatch(dotaClient.client.gsi)) return
 
-    logger.info('Chat message event received', { event, token: dotaClient.client.token })
-
-    const message = ''
+    const message = moderateText(event.message?.trim())
     if (!message || typeof message !== 'string') return
 
-    if (isRussian(message)) {
+    const detectedLang = franc(message)
+    if (detectedLang !== 'eng' && detectedLang !== 'und') {
       try {
         const { text } = await translate(message, { to: 'en' })
-        // Say the translated message in chat
+        server.io.to(dotaClient.getToken()).emit('chatMessage', moderateText(text))
+
+        // TODO: maybe say the translated message in chat
         // say(dotaClient.client, text)
       } catch (error) {
-        logger.error('Translation error:', error)
+        logger.error('Translation error:', { error, message })
       }
     }
   },
