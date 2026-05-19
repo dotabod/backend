225c2c2533536d8dfc3d48d01a1aabf8f4581193
Matt Gates <Geczy@users.noreply.github.com>
2026-05-19 16:04:03 -0500

    Exit dota cleanly on SIGTERM so bun --cpu-prof flushes

--- packages/dota/src/index.ts
+++ packages/dota/src/index.ts
@@ -1,3 +1,6 @@
+process.on('SIGTERM', () => process.exit(0))
+process.on('SIGINT', () => process.exit(0))
+
 function initServer() {
   Promise.all([import('./dota/index'), import('./twitch/index')])
     .then(() => {
